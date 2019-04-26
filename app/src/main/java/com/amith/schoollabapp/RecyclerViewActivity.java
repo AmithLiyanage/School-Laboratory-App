11ed60a22dd54dc46eb5fb9677b196cdbb815f3d
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-04-26 09:51:18 +0530

    correction

--- app/src/main/java/com/amith/schoollabapp/RecyclerViewActivity.java
+++ app/src/main/java/com/amith/schoollabapp/RecyclerViewActivity.java
@@ -1,6 +1,7 @@
 package com.amith.schoollabapp;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -26,20 +27,28 @@ public class RecyclerViewActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("RECYCLER VIEW");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        firstItem = new ArrayList<>();
-        firstItem.add(new MenuItem("Glassware", "'Equipments", "Description", R.drawable.smart_lab_logo));
-        firstItem.add(new MenuItem("Chemicals", "'Equipments", "Description", R.drawable.smart_lab_logo));
-        firstItem.add(new MenuItem("Perishables", "'Equipments", "Description", R.drawable.smart_lab_logo));
-        firstItem.add(new MenuItem("Permanent Eq", "'Equipments", "Description", R.drawable.smart_lab_logo));
-        firstItem.add(new MenuItem("Glassware", "'Equipments", "Description", R.drawable.smart_lab_logo));
-        firstItem.add(new MenuItem("Chemicals", "'Equipments", "Description", R.drawable.smart_lab_logo));
-        firstItem.add(new MenuItem("Perishables", "'Equipments", "Description", R.drawable.smart_lab_logo));
-        firstItem.add(new MenuItem("Permanent Eq", "'Equipments", "Description", R.drawable.smart_lab_logo));
+        Toast.makeText(this, "OK", Toast.LENGTH_SHORT ).show();
 
-        RecyclerView myrv = (RecyclerView) findViewById(R.id.recycler_view);
-        RecycleViewAdapter myAdapter = new RecycleViewAdapter(this, firstItem);
-        myrv.setLayoutManager(new GridLayoutManager(this,3));
-        myrv.setAdapter(myAdapter);
+        firstItem = new ArrayList<>();
+        firstItem.add(new MenuItem("Glassware", "'Equipments", "Description", R.drawable.ic_menu_camera));
+        firstItem.add(new MenuItem("Chemicals", "'Equipments", "Description", R.drawable.ic_menu_camera));
+        firstItem.add(new MenuItem("Perishables", "'Equipments", "Description", R.drawable.ic_menu_camera));
+        firstItem.add(new MenuItem("Permanent Eq", "'Equipments", "Description", R.drawable.ic_menu_camera));
+        firstItem.add(new MenuItem("Glassware", "'Equipments", "Description", R.drawable.ic_menu_camera));
+        firstItem.add(new MenuItem("Chemicals", "'Equipments", "Description", R.drawable.ic_menu_camera));
+        firstItem.add(new MenuItem("Perishables", "'Equipments", "Description", R.drawable.ic_menu_camera));
+        firstItem.add(new MenuItem("Permanent Eq", "'Equipments", "Description", R.drawable.ic_menu_camera));
+
+        try {
+            Toast.makeText(this, "IN the Recycler view", Toast.LENGTH_SHORT ).show();
+            RecyclerView myrv = (RecyclerView) findViewById(R.id.recycler_view);
+            RecycleViewAdapter myAdapter = new RecycleViewAdapter(this, firstItem);
+            myrv.setLayoutManager(new GridLayoutManager(this,3));
+            myrv.setAdapter(myAdapter);
+        } catch (Exception e) {
+            Toast.makeText(this, "Recycler error : "+e, Toast.LENGTH_LONG ).show();
+        }
+
     }
 }
 
