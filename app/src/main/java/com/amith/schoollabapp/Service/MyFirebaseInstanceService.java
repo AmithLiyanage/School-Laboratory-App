1e79d047a8d6a783bd164609d2d0aaf1ea25a1cd
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-05-10 20:47:25 +0530

    end notification part(not working killed part)

--- app/src/main/java/com/amith/schoollabapp/Service/MyFirebaseInstanceService.java
+++ app/src/main/java/com/amith/schoollabapp/Service/MyFirebaseInstanceService.java
@@ -14,6 +14,7 @@ import com.amith.schoollabapp.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
 import java.util.Random;
 
 public class MyFirebaseInstanceService extends FirebaseMessagingService {
@@ -25,8 +26,45 @@ public class MyFirebaseInstanceService extends FirebaseMessagingService {
 
         super.onMessageReceived(remoteMessage);
 
-        showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        if (remoteMessage.getData().isEmpty()) {
+            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        } else {
+            showNotification(remoteMessage.getData());
+        }
         
+    }
+
+    private void showNotification(Map<String, String> data) {
+
+        String title = data.get("title"); //.toString() removed at last
+        String body = data.get("body");//.toString() removed at last
+
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "com.amith.schoollabapp.test";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification", NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("EDMT Channel");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info");
+
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
     }
 
     private void showNotification(String title, String body) {
@@ -55,7 +93,6 @@ public class MyFirebaseInstanceService extends FirebaseMessagingService {
                 .setContentInfo("Info");
 
         notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
-
     }
 
     @Override
