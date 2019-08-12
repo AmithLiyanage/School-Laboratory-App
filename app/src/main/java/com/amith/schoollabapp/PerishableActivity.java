e9c0da05ae09124922f147a5089a4b081f97ea25
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-08-13 01:37:28 +0530

    basically completed perishables and permenent equip views

--- app/src/main/java/com/amith/schoollabapp/PerishableActivity.java
+++ app/src/main/java/com/amith/schoollabapp/PerishableActivity.java
@@ -8,12 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.amith.schoollabapp.Adapter.ChemicalAdapter;
-import com.amith.schoollabapp.Model.Chemical;
+import com.amith.schoollabapp.Adapter.PerishableAdapter;
+import com.amith.schoollabapp.Model.Perishable;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,35 +23,36 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class ChemicalsActivity extends AppCompatActivity {
+public class PerishableActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private ChemicalAdapter chemicalAdapter;
+    private PerishableAdapter perishableAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
-    private ArrayList<Chemical> mChemicals;
+    private ArrayList<Perishable> mPerishable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chemicals);
+        setContentView(R.layout.activity_perishable);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Chemicals");
+        getSupportActionBar().setTitle("Perishables");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(ChemicalsActivity.this, NavigationActivity.class).setFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP));
+                startActivity(new Intent(PerishableActivity.this, NavigationActivity.class).setFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP));
             }
         });
 //
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_chemical_view);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_perishable_view);
         recyclerView.setHasFixedSize(true);
 
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        mChemicals = new ArrayList<>();
+        mPerishable = new ArrayList<>();
 
         readChemicals();
 
@@ -60,32 +60,32 @@ public class ChemicalsActivity extends AppCompatActivity {
 
     private void readChemicals() {
 
-        Toast.makeText(ChemicalsActivity.this.getBaseContext(), "0", Toast.LENGTH_SHORT);
+        Toast.makeText(PerishableActivity.this.getBaseContext(), "0", Toast.LENGTH_SHORT);
         final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("chemicals");
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("perishables");
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                mChemicals.clear();
+                mPerishable.clear();
 
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Chemical chemical = snapshot.getValue(Chemical.class);
+                    Perishable chemical = snapshot.getValue(Perishable.class);
 
-                    Toast.makeText(ChemicalsActivity.this.getBaseContext(), "1", Toast.LENGTH_SHORT);
+                    Toast.makeText(PerishableActivity.this.getBaseContext(), "1", Toast.LENGTH_SHORT);
                     assert chemical != null;
 //                    Log.d("dsa", ""+chemical.getItem_name());
-                    mChemicals.add(chemical);
-                    Toast.makeText(ChemicalsActivity.this.getBaseContext(), "2", Toast.LENGTH_LONG);
+                    mPerishable.add(chemical);
+                    Toast.makeText(PerishableActivity.this.getBaseContext(), "2", Toast.LENGTH_LONG);
                 }
 //                mChemicals.add(new Chemical("dsad","chem name", (long)2,(long)2.0,"g"));
 
                 try {
-                    chemicalAdapter = new ChemicalAdapter(getBaseContext(), mChemicals);
-                    recyclerView.setAdapter(chemicalAdapter);
+                    perishableAdapter = new PerishableAdapter(getBaseContext(), mPerishable);
+                    recyclerView.setAdapter(perishableAdapter);
                 } catch (Exception e) {
-                    Toast.makeText(ChemicalsActivity.this.getBaseContext(), "Chemical loading Error : "+e.getMessage(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(PerishableActivity.this.getBaseContext(), "Perishable loading Error : "+e.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
 
--- app/src/main/java/com/amith/schoollabapp/PermanentEquipmentActivity.java
+++ app/src/main/java/com/amith/schoollabapp/PermanentEquipmentActivity.java
@@ -8,12 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.amith.schoollabapp.Adapter.ChemicalAdapter;
-import com.amith.schoollabapp.Model.Chemical;
+import com.amith.schoollabapp.Adapter.PermanentEquipmentAdapter;
+import com.amith.schoollabapp.Model.PermanentEquipment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,68 +23,71 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class ChemicalsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class PermanentEquipmentActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private ChemicalAdapter chemicalAdapter;
+    private PermanentEquipmentAdapter permanentEquipmentAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
-    private ArrayList<Chemical> mChemicals;
+    private ArrayList<PermanentEquipment> mPermanentEquipment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chemicals);
+        setContentView(R.layout.activity_permanent_equipment);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Chemicals");
+        getSupportActionBar().setTitle("Permanent Equipments");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(ChemicalsActivity.this, NavigationActivity.class).setFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP));
+                startActivity(new Intent(PermanentEquipmentActivity.this, NavigationActivity.class).setFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP));
             }
         });
 //
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_chemical_view);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_permanent_equipment_view);
         recyclerView.setHasFixedSize(true);
 
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        mChemicals = new ArrayList<>();
+        mPermanentEquipment = new ArrayList<>();
 
-        readChemicals();
+        readPermanentEquipment();
 
     }
 
-    private void readChemicals() {
+    private void readPermanentEquipment() {
 
-        Toast.makeText(ChemicalsActivity.this.getBaseContext(), "0", Toast.LENGTH_SHORT);
+        Toast.makeText(PermanentEquipmentActivity.this.getBaseContext(), "0", Toast.LENGTH_SHORT);
         final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("chemicals");
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("permenant_equipment");//dont change spellings, thees fron  DB
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                mChemicals.clear();
+                mPermanentEquipment.clear();
 
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Chemical chemical = snapshot.getValue(Chemical.class);
+                    PermanentEquipment chemical = snapshot.getValue(PermanentEquipment.class);
 
-                    Toast.makeText(ChemicalsActivity.this.getBaseContext(), "1", Toast.LENGTH_SHORT);
+                    Toast.makeText(PermanentEquipmentActivity.this.getBaseContext(), "1", Toast.LENGTH_SHORT);
                     assert chemical != null;
 //                    Log.d("dsa", ""+chemical.getItem_name());
-                    mChemicals.add(chemical);
-                    Toast.makeText(ChemicalsActivity.this.getBaseContext(), "2", Toast.LENGTH_LONG);
+                    mPermanentEquipment.add(chemical);
+                    Toast.makeText(PermanentEquipmentActivity.this.getBaseContext(), "2", Toast.LENGTH_LONG);
                 }
 //                mChemicals.add(new Chemical("dsad","chem name", (long)2,(long)2.0,"g"));
 
                 try {
-                    chemicalAdapter = new ChemicalAdapter(getBaseContext(), mChemicals);
-                    recyclerView.setAdapter(chemicalAdapter);
+                    permanentEquipmentAdapter = new PermanentEquipmentAdapter(getBaseContext(), mPermanentEquipment);
+                    recyclerView.setAdapter(permanentEquipmentAdapter);
                 } catch (Exception e) {
-                    Toast.makeText(ChemicalsActivity.this.getBaseContext(), "Chemical loading Error : "+e.getMessage(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(PermanentEquipmentActivity.this.getBaseContext(), "PermanentEquipment loading Error : "+e.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
 
